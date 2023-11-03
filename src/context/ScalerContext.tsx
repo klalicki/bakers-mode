7780a5c28e2dd61225563f675afbccffe1ec259c
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-11-02 23:25:35 -0400

    implements basic scaling function

--- src/context/ScalerContext.tsx
+++ src/context/ScalerContext.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, createContext, useState } from "react";
+import { PropsWithChildren, createContext, useEffect, useState } from "react";
 import { sampleData } from "../data/sampleData";
 import { RecipeData, blankRecipe } from "../types/RecipeDataTypes";
 const ScalerContext = createContext({});
@@ -6,13 +6,47 @@ const ScalerContext = createContext({});
 export const ScalerWrapper = (props: PropsWithChildren) => {
   const [recipe, setRecipe] = useState(blankRecipe);
   const [scaledRecipe, setScaledRecipe] = useState<RecipeData>(blankRecipe);
+  const [recipeWeight, setRecipeWeight] = useState(0);
+  const [targetWeight, setTargetWeight] = useState(3102);
 
+  // function that loads a new recipe into the context object
+  // handles calculating the total weight of the original recipe for the scaling function
   const loadRecipe = (newRecipe: RecipeData) => {
-    const tempRecipe = { ...newRecipe };
-    console.log(tempRecipe);
-  };
+    // calculate the total weight of the original recipe
+    const totalWeight = newRecipe.steps
+      .map((step) => {
+        // extract the ingredient quantities from each step
+        return step.ingredients.map((ingredient) => {
+          return ingredient.qty;
+        });
+      })
+      .flat()
+      .reduce((item1, item2) => {
+        return item1 + item2;
+      });
+    setRecipe(newRecipe);
+    setRecipeWeight(totalWeight);
 
-  loadRecipe(sampleData);
+    console.log(totalWeight);
+  };
+  // scales the recipe to the new target weight and saves it to scaledRecipe
+  const scaleRecipe = () => {
+    const newSteps = recipe.steps.map((step) => {
+      const newIngredients = step.ingredients.map((ingredient) => {
+        return {
+          name: ingredient.name,
+          qty: (ingredient.qty / recipeWeight) * targetWeight,
+        };
+      });
+      return { ...step, ingredients: newIngredients };
+    });
+    setScaledRecipe({ ...recipe, steps: newSteps });
+  };
+  useEffect(() => {
+    loadRecipe(sampleData);
+    scaleRecipe();
+    console.log(scaledRecipe);
+  });
   return (
     <ScalerContext.Provider value={{ scaledRecipe, loadRecipe }}>
       {props.children}
