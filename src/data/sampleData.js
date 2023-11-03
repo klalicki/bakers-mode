e831191ea21a32a8a685ac7b811ab2e6989e3982
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-11-02 20:49:49 -0400

    adds sample data

---
+++ src/data/sampleData.js
@@ -0,0 +1,45 @@
+export const sampleData = {
+  title: "My First Pizza",
+  description: "This is a sample pizza recipe",
+  steps: [
+    {
+      title: "Wet ingredients",
+      ingredients: [
+        { qty: 552, name: "water" },
+        { qty: 24, name: "salt" },
+        { qty: 9, name: "sugar" },
+        { qty: 6, name: "instant yeast" },
+      ],
+      text: "combine these ingredients in stand mixer bowl, stir with dough hook.",
+    },
+    {
+      title: "Flours",
+      ingredients: [
+        { qty: 880, name: "00 pizza flour" },
+        { qty: 40, name: "wheat gluten" },
+        { qty: 40, name: "all-purpose flour" },
+      ],
+      text: "add flours to liquid, and mix with dough hook on speed 4 for 10 minutes. The dough will be relatively wet.",
+    },
+    {
+      title: "Bulk Ferment",
+      ingredients: [],
+      text: "Cover bowl, and rise at room temperature for 2 hours.",
+    },
+    {
+      title: "Scaling",
+      ingredients: [],
+      text: "Scale dough to individual pizzas, and put in oiled containers with lids",
+    },
+    {
+      title: "Proofing",
+      ingredients: [],
+      text: "Proof dough for at least 30 minutes while the oven preheats",
+    },
+  ],
+  cookingNotes: [
+    "dunk each ball of dough in 00 flour before stretching.",
+    "launch directly onto pizza stone.",
+    "cooks well at 650F for 5-6 minutes.",
+  ],
+};
