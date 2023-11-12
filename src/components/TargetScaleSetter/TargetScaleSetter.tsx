5097b5585f14a5d18ee0d1cc34df01394951edfd
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-11-12 13:36:05 -0500

    refactors TargetScaleSetter to be more generic

--- src/components/TargetScaleSetter/TargetScaleSetter.tsx
+++ src/components/TargetScaleSetter/TargetScaleSetter.tsx
@@ -6,34 +6,31 @@ export const TargetScaleSetter = () => {
     ScalerContext
   ) as ScalerContextType;
 
-  const [pizzaCount, setPizzaCount] = useState<string | number>(4);
-  const [pizzaSize, setPizzaSize] = useState<string | number>(250);
+  const [unitCount, setUnitCount] = useState<string | number>(4);
+  const [unitWeight, setUnitWeight] = useState<string | number>(250);
 
   useEffect(() => {
-    setTargetWeight((pizzaSize as number) * (pizzaCount as number));
-  }, [pizzaCount, pizzaSize, setTargetWeight]);
+    setTargetWeight((unitWeight as number) * (unitCount as number));
+  }, [unitCount, unitWeight, setTargetWeight]);
   return (
     <article className=" w-full border p-2">
       <h2>Scaling</h2>
       <div className="flex gap-2 flex-wrap">
         <NumberInput
-          className=" w-60"
-          value={pizzaCount}
-          label={"Number of Pizzas"}
-          onChange={setPizzaCount}
+          className=" w-72"
+          value={unitCount}
+          label={"Number of units (pizza, loaf, etc)"}
+          onChange={setUnitCount}
         />{" "}
         <NumberInput
-          className=" w-60"
-          value={pizzaSize}
-          label={"Dough per pizza"}
-          onChange={setPizzaSize}
+          className=" w-72"
+          value={unitWeight}
+          label={"Dough Weight per unit (g)"}
+          onChange={setUnitWeight}
         />
       </div>
 
-      <h2>
-        total weight:
-        {targetWeight}
-      </h2>
+      <p aria-live="polite">total weight: {targetWeight}g</p>
     </article>
   );
 };
